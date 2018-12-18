b66a4b884486f147b92363f258f684fdfb601893
fosteradorkor <fosteradorkor@icloud.com>
2018-12-18 16:01:26 +0000

    new

--- fakye.web-cartel.com/js/custom/listing.js
+++
@@ -1,5 +0,0 @@
-
-
-/* Create new Printer Item */
-
-
--- fakye.web-cartel.com/js/custom/pages.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(".categories_btn").click(function(){
-    	$(".admin_dashboard").empty();
-        $(".admin_dashboard").load("/manage-categories");
-    });
-});
